8d7fab9de37b9aec853ee26b03bcb6c098b721a2
Joe Lau <joe.lau@neoderm.com.hk>
2022-02-25 19:08:34 +0800

    Wire up apollo server express

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
 const express = require('express');
+const http = require('http');
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
 
-const app = express();
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+  });
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000...');
-})
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise(resolve => httpServer.listen({ port : 4000 }, resolve));
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+startApolloServer(typeDefs, resolvers);
